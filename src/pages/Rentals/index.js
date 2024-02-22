42faa5ecf3ad6cea9da1886191ba22553f691d7e
Hania Imran <hanna.khan205@gmail.com>
2024-02-22 17:36:56 +0500

    rental styling

--- src/pages/Rentals/index.js
+++ src/pages/Rentals/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useState } from "react";
 import images from "../../utils/Images";
 import styles from "./style.module.css";
@@ -510,36 +511,40 @@ const Rentals = () => {
   console.log(value);
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.textDiv}>
-          <div className={styles.text1}>{`HOME > RENTALS`}</div>
-          <div className={styles.text2}>Rentals</div>
-          <div className={styles.text3}>
+      <div className="w-full">
+        <div className="w-full bg-[#F4F4F4] flex flex-col justify-center items-center h-[16rem]">
+          <div className="text-[#807D7E] font-500 text-[0.7rem] items-center tracking-[2px]">{`HOME > RENTALS`}</div>
+          <div className="text-[#000] font-700 text-[2rem] items-center mt-2 font-bold mb-2">
+            Rentals
+          </div>
+          <div className="text-[#6D6E76] font-500 text-sm items-center w-[40%] my-0 mx-auto mt-2 justify-center text-center leading-6">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore.
           </div>
         </div>
-        <div className={styles.containerTwo}>
-          <div className={styles.secondDiv}>
+        <div className="py-0 px-[3%]">
+          <div className="w-full flex flex-row pt-10">
             {showFilter ? (
-              <div className={styles.showFilterDiv}>
-                <div className={styles.showFilterDivOne}>
-                  <span>Filter</span>
+              <div className="w-[25%] mt-10 relative left-[1.5rem]">
+                <div className="w-[80%] flex flex-row items-center justify-between mx-0 my-auto border-b-1 border-solid border-b-[#BEBCBD] pb-5">
+                  <span className="text-[#ffba3f] font-semibold text-[1.2rem] items-center">
+                    Filter
+                  </span>
                   <img
                     onClick={() => setShowFilter(false)}
                     src={images.filter}
-                    className={styles.hideFilterDivImg}
+                    className="w-[20px] h-[22px] cursor-pointer"
                   />
                 </div>
-                <div className={styles.showFilterDivTwo}>
+                <div className="w-[80%] flex flex-col mx-0 my-auto pb-2">
                   <div>Categories</div>
                   <span>All</span>
                   <span>Hydrolic Pumps</span>
                   <span>Engine</span>
                   <span>Filters</span>
                 </div>
-                <div className={styles.showFilterDivThree}>
-                  <div>Price</div>
+                <div className="w-[80%] flex flex-col mx-0 my-auto pb-6 mt-8">
+                  <div className="text-[#807d7e] text-md font-600">Price</div>
                   <Slider
                     getAriaLabel={() => "Temperature range"}
                     valueLabelDisplay="auto"
@@ -548,60 +553,60 @@ const Rentals = () => {
                     onChange={handleChange}
                   />
                 </div>
-                <div className={styles.showFilterDivFour}>
+                <div className="w-[80%] flex flex-row items-center mx-0 my-auto justify-between mt-8">
                   <div>70</div>
                   <div>600</div>
                 </div>
               </div>
             ) : (
-              <div className={styles.hideFilterDiv}>
+              <div className="w-[15%] flex flex-col mt-10 items-center">
                 <img
                   src={images.filter}
-                  className={styles.hideFilterDivImg}
+                  className="w-5 h-5 cursor-pointer"
                   onClick={() => setShowFilter(true)}
                 />
-                <hr className={styles.hideFilterDivHr} />
+                <hr className="w-[23px] border-none h-[1px] mt-3 bg-[#BEBCBD]" />
               </div>
             )}
-            <div className={styles.secondDivSecond}>
-              <div className={styles.searchMainDiv}>
-                <div className={styles.searchDiv}>
-                  <img src={images.search} className={styles.searchImg} />
-                  <input placeholder="Search" className={styles.searchInput} />
+            {/* start */}
+            <div className="w-[90%] flex flex-col bg-white px-0 py-8 box-border pb-10">
+              <div className="w-full flex flex-row items-end justify-between">
+                <div className="w-3/12 bg-gray-200 rounded-md flex items-center">
+                  <img src={images.search} className="ml-3 w-5 h-5" />
+                  <input
+                    placeholder="Search"
+                    className="w-full bg-transparent ml-2 focus:outline-none"
+                  />
                 </div>
                 <span>{`Showing ${currentPage}-${currentItems.length} of ${data?.length} results`}</span>
               </div>
-              <div className={styles.mapMainDiv}>
+              <div className="flex flex-wrap justify-between mt-8">
                 {currentItems.map((item, index) => {
                   return (
-                    <div key={index} className={styles.readDetailsCard}>
-                      <div className={styles.readDetailsCardRow}>
-                        <div className={styles.readDetailsCardRowRight}>
-                          <img
-                            className={styles.cardLocationImg}
-                            src={images.location}
-                          />
-                          <div className={styles.cardLocationText}>
+                    <div
+                      key={index}
+                      className="w-[30%] bg-white rounded-lg shadow-md p-4 mb-8"
+                    >
+                      <div className="flex justify-between">
+                        <div>
+                          <img className="w-4 h-4" src={images.location} />
+                          <span className="text-sm text-gray-500 ml-1">
                             {item.location}
-                          </div>
+                          </span>
                         </div>
-                        <div className={styles.cardTextBold}>
+                        <div className="font-bold text-black">
                           Rs {item.price}
                         </div>
                       </div>
-                      <div className={styles.readDetailsCardRow}>
-                        <div className={styles.cardTextBold}>{item.name}</div>
-                        <div className={styles.cardDayText}>per day</div>
+                      <div className="flex justify-between mt-2">
+                        <div className="font-bold">{item.name}</div>
+                        <div className="text-gray-500 text-sm">per day</div>
                       </div>
-                      <img className={styles.cardImage} src={item.image[0]} />
-                      <div className={styles.cardCommentText}>
-                        {item.comment}
-                      </div>
+                      <img className="w-full mt-2" src={item.image[0]} />
+                      <div className="text-gray-600 mt-2">{item.comment}</div>
                       <div
-                        className={styles.readDetailsBtn}
-                        onClick={() => {
-                          handleNavigation(item);
-                        }}
+                        className="bg-yellow-500 text-white font-semibold rounded-md text-center mt-4 py-2 cursor-pointer"
+                        onClick={() => handleNavigation(item)}
                       >
                         Read Details
                       </div>
@@ -610,9 +615,11 @@ const Rentals = () => {
                 })}
               </div>
             </div>
+
+            {/* end */}
           </div>
         </div>
-        <div className={styles.paginationDiv}>
+        <div className="flex justify-center items-center mt-8">
           <UserPagination
             itemsPerPage={itemsPerPage}
             totalItems={data.length}
