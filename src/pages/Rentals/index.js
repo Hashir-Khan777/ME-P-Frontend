61fdc5c5682b50308ba85aa3da90bb5e692d627b
Hania Imran <hanna.khan205@gmail.com>
2024-02-23 23:47:36 +0500

    rentals responsiveness fixed

--- src/pages/Rentals/index.js
+++ src/pages/Rentals/index.js
@@ -515,7 +515,7 @@ const Rentals = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className="w-full bg-[#F4F4F4] flex flex-col justify-center items-center h-[16rem] mb-16">
+        <div className="w-full bg-[#F4F4F4] flex flex-col justify-center items-center h-[16rem] sm:mb-16 mb-0">
           <div className="text-[#807D7E] font-500 text-[0.7rem] items-center tracking-[2px]">{`HOME > RENTALS`}</div>
           <div className="text-[#000] font-700 text-[1.8rem] sm:text-[2rem] items-center mt-2 font-bold mb-2">
             Rentals
