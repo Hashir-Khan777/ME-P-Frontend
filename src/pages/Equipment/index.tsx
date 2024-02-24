3ea11283137322420dae3e19414d96b9dd12da8d
Hania Imran <hanna.khan205@gmail.com>
2024-02-24 16:50:18 +0500

    equipment changes files deleted

--- src/pages/Equipment/index.tsx
+++ src/pages/Equipment/index.tsx
@@ -1,20 +1,20 @@
-import React, {useState} from "react";
-import ProductFilter from "../../components/Product Filter";
+import React, { useState } from "react";
 import Header from "../Rentals/Header";
 import UserPagination from "../../components/UserPagination";
 import styles from "../Rentals/style.module.css";
 import { data } from "../Rentals/ProductsData";
+import FilterView from "../Rentals/FilterView";
 
 const Equipment = () => {
-  const [currentPage, setCurrentPage ] = useState(1);
-  const [itemsPerPage, ] = useState(3);
-  const handlePageChange = (pageNumber:any) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage,] = useState(3);
+  const handlePageChange = (pageNumber: any) => {
     setCurrentPage(pageNumber);
   };
   return (
     <>
       <Header title={"Equipment"} para={"Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."} subTitle={"HOME > EQUIPMENT"} />
-      <ProductFilter />
+      <FilterView data={data} />
       <div className={styles.paginationDiv}>
         <UserPagination
           itemsPerPage={itemsPerPage}
